d5040841fa7ca0e2679775e6d4c47224e3716d4b
Lavken <lavkenpilar@gmail.com>
2024-02-10 02:00:43 -0300

    create-index-js

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let nombres = [
+    "Juan",
+    "Luisa",
+    "Fabian",
+    "Jorge",
+    "Elon",
+    "Steve",
+    "Bill",
+    "Estefany"
+]
+console.log (nombres [0])
+console.log (nombres[5])
+console.log (nombres [7])
